dab0505b3c2629677380190b41c42e40bc7116a0
Sebastian Albers <seb.albers@gmail.com>
2020-02-15 20:16:53 +0100

    Moved all pushes to Docker Hub.

--- generate-dockerfiles.sh
+++ generate-dockerfiles.sh
@@ -6,7 +6,7 @@ for docker_arch in amd64 arm32v7 arm64v8; do
   case ${docker_arch} in
     amd64   ) qemu_arch="x86_64" ;;
     arm32v7 ) qemu_arch="arm" ;;
-    arm64v8 ) qemu_arch="aarch64" ;;    
+    arm64v8 ) qemu_arch="aarch64" ;;
   esac
   cp Dockerfile.cross Dockerfile.${docker_arch}
   sed -i'' "s|__BASEIMAGE_ARCH__|${docker_arch}|g" Dockerfile.${docker_arch}
@@ -17,4 +17,3 @@ for docker_arch in amd64 arm32v7 arm64v8; do
     sed -i'' "s/__CROSS_//g" Dockerfile.${docker_arch}
   fi
 done
-
