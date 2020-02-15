efd57f2331db972ef3ffb3f1a5afd36f6fa37d24
Sebastian Albers <seb.albers@gmail.com>
2020-02-15 15:47:31 +0100

    Updated read-version script.

--- read-version.sh
+++ read-version.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+set -eou pipefail
+
 docker run sebastianalbers/tor:latest-$1 /usr/bin/tor --version | \
   sed '/Tor version/!{q1}; s/^Tor version \([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*\?$/\1/'
