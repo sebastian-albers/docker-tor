3700eef3229d2709f26b953ab1fe5d492254eaeb
Sebastian Albers <seb.albers@gmail.com>
2020-02-15 13:57:43 +0100

    Fixed read-version script.

--- read-version.sh
+++ read-version.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run sebastianalbers/rpi-tor:latest-$1 /usr/bin/tor --version | \
-  sed 's/^Tor version \([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*\?$/\1/'
+docker run sebastianalbers/tor:latest-$1 /usr/bin/tor --version | \
+  sed '/Tor version/!{q1}; s/^Tor version \([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*\?$/\1/'
