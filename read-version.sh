4bb9d819af1407eca62d3949ed8a0f64f8b5a926
Sebastian Albers <seb.albers@gmail.com>
2020-06-23 20:57:53 +0200

    Fixed read-version script.

--- read-version.sh
+++ read-version.sh
@@ -2,5 +2,5 @@
 
 set -eou pipefail
 
-docker run sebastianalbers/tor:latest-$1 /usr/bin/tor --version | \
+docker run sebastianalbers/tor:latest-$1 /usr/bin/tor --version | tail -n 1 | \
   sed '/^Tor version/!{q1}; s/^Tor version \([0-9]*\.[0-9]*\.[0-9]*\.[0-9]*\).*\?$/\1/'
